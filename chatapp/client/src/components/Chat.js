bc85e1f6004b349c843ea88c274726cd89866083
SaeedQatanani <saeedqatananil@gmail.com>
2022-12-25 13:40:59 +0200

    scrolled down and welcome message added

--- chatapp/client/src/components/Chat.js
+++ chatapp/client/src/components/Chat.js
@@ -1,51 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import io from 'socket.io-client'
-import axios from 'axios'
 
 const Chat = (props) => {
     const [socket] = useState(() => io(':8000'));
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
-    // const [notification, setNotification] = useState("");
+    const bottomRef = useRef(null);
 
     useEffect(() => {
         console.log("Running");
         socket.on("welcome", data => console.log(data));
-        socket.on("message_to_chat", data => setMessages(prevMessages => {
-            return [...prevMessages, data];
-        }));
-        socket.on("all_msgs_back", data => setMessages(prevMessages => {
-            return [...prevMessages, data];
-        }));
-        // return () => socket.disconnect(true);
-    }, []);
+        socket.on("messages_to_chat", data => setMessages(data));
+    }, [socket]);
+
+    useEffect(() => {
+        socket.emit("new_user",  props.name);
+    }, [props.name, socket]);
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+
     const handelSubmit = e => {
         e.preventDefault();
         socket.emit("message_from_client", { msg: message, name: props.name });
-        axios.post('http://localhost:8000/api/', { msg: message, name: props.name })
-        .then(res => console.log(res))
         setMessage("");
     }
     return (
-        <div style={{ width: "82%", maxHeight: "500px", overflow: "scroll", margin: "0 auto", border: "1px solid black" }}>
+        <div style={{ width: "82%", maxHeight: "300px", overflow: "auto", margin: "0 auto", border: "1px solid black" }}>
             <h1>Welcome, {props.name}</h1>
             {
                 messages.map((msg, i) =>
-                    (msg.name == props.name)?
-                    <div key={i} style={{ width: "40%", margin: "0 0 0 auto", border: "1px solid red" }}>
-                        {msg.name} { msg.name?<>said: </>:<></>} {msg.msg}
-                    </div>
-                    :
-                    <div key={i} style={{ width: "40%", margin: "0 auto 0 0", border: "1px solid red" }}>
-                        {msg.name} { msg.name?<>said: </>:<></>} {msg.msg}
-                    </div>
-
+                    (msg.name === props.name) ?
+                        <div key={i} style={{ width: "40%", margin: "1% 0 1% auto", border: "1px solid black", backgroundColor: "#9fc5f8", boxShadow: "3px 3px 3px #777", borderRadius: "10px" }}>
+                            <span><strong>{msg.name}: </strong></span><br /> {msg.msg}
+                        </div>
+                        :
+                        <div key={i} style={{ width: "40%", margin: "1% auto 1% 0", border: "1px solid black", backgroundColor: "#dddddd", boxShadow: "3px 3px 3px #777", borderRadius: "10px" }}>
+                            <span><strong>{msg.name}</strong></span> {msg.name ? <>said: </> : <></>} <br /> {msg.msg}
+                        </div>
                 )
             }
             <form onSubmit={handelSubmit}>
                 <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
                 <input type="submit" value="Send" />
             </form>
+            <div ref={bottomRef} />
         </div>
     )
 }
--- chatapp/server/server.js
+++ chatapp/server/server.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const server = app.listen(8000, () =>
     console.log('The server is all fired up on port 8000')
 );
 const io = require('socket.io')(server, { cors: true });
 
-var msgs = [];
-app.post("/api", (req, res) => {
-    msgs.push(req.body);
-    res.json({ status: "ok" });
-});
-
+const msgs = [];
 io.on("connection", socket => {
     console.log("Nice to meet me.");
     socket.emit("welcome", "Welcome to our socket!");
+    io.emit("messages_to_chat", msgs);
     socket.on("message_from_client", data => {
-        msgs = [...msgs, data];
-        io.emit("message_to_chat", data);
+        msgs.push(data);
+        io.emit("messages_to_chat", msgs);
     });
-    socket.emit("all_msgs_back", msgs);
+    socket.on("new_user", data => {
+        msgs.push({msg:data+" has joined the chat"});
+        io.emit("messages_to_chat", msgs);
+    })
 });
